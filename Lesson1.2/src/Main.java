e238b178396011743d64bdc13f3fd92549842605
PowerlineR413 <Powerline413@yandex.ru>
2024-07-26 17:00:56 +0300

    Добавление теста 2

---
+++ Lesson1.2/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author El Baron 09.07.2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*****************************");
+        System.out.println("****       Визитка       ****");
+        System.out.println("**** Name: Дима          ****");
+        System.out.println("**** Skype: paladinre413 ****");
+        System.out.println("*****************************");
+
+
+    }
+}
