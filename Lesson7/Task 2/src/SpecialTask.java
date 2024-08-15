8f613e60613e3659d00afb2980b71f2e1f6e992e
PowerlineR413 <Powerline413@yandex.ru>
2024-08-15 21:11:19 +0300

    Задача особой сложности

---
+++ Lesson7/Task 2/src/SpecialTask.java
@@ -0,0 +1,12 @@
+/**
+ * @author El Baron 15.08.2024
+ */
+public class SpecialTask {
+    public static void main(String[] args) {
+        int poorMen = 0;
+        while (poorMen < 100) {
+            poorMen = poorMen + 2;
+            System.out.println("Уволен айтишник с ID " + poorMen);
+        }
+    }
+}
