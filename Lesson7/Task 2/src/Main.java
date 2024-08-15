1c0e1b9d6d3ac934c834521d1b3b178b7e0fd9fe
PowerlineR413 <Powerline413@yandex.ru>
2024-08-15 16:37:21 +0300

    Вариант 1.1, позже переделаю когда буду знать как лучше

---
+++ Lesson7/Task 2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //todo Тут увольняем айтишников
+
+        for (int poorMensCounter = 0; poorMensCounter < 100;){
+            poorMensCounter = poorMensCounter + 2;
+            System.out.println("Уволен айтишник с ID "+poorMensCounter);
+        }
+
+
+    }
+}
