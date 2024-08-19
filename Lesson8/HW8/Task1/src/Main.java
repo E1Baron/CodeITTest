8cc6b24f669c0f9495298ad6aa407e4ae71dd4b1
PowerlineR413 <Powerline413@yandex.ru>
2024-08-19 17:55:30 +0300

    Выполнил задачу с реверсом массив и навёл некоторый порядок в коде

--- Lesson8/HW8/Task1/src/Main.java
+++ Lesson8/HW8/Task1/src/Main.java
@@ -5,28 +5,30 @@ public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
+
         int[] testArray = new int[]{10, 12, 13, 34, 55, 33, 49, 59, 14, 79};
-        System.out.println("Изначальный массив: " + Arrays.toString(testArray));
-//
-//        //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-//        //todo
-//        for (int i = 0; i < testArray.length; i++) {
-//            System.out.print(testArray[i] + " ");
-//        }
-//        System.out.println();
-//        //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-//        //todo
-//        for (int i = testArray.length - 1; i >= 0; i--) {
-//            System.out.print(testArray[i] + " ");
-//        }
-//        System.out.println();
-//
-//        //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-//        //todo
-////        int backUp = 0;
-////        backUp = testArray[0];
-////        testArray[0] = testArray[10];
-////        testArray[10] = backUp;
+        System.out.println();
+
+        //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
+        //todo
+
+        for (int i = 0; i < testArray.length; i++) {
+            System.out.print(testArray[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Распечатать все значения массива начиная с 0 до последнего индексов");
+        System.out.println();
+        //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
+        //todo
+        for (int i = testArray.length - 1; i >= 0; i--) {
+            System.out.print(testArray[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Распечатать все значения массива с конца (начиная с последнего до 0 индексов)");
+        System.out.println();
+
+        //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
+        //todo
         int[] testArrayRevers = new int[testArray.length];
         int j = 0;
         for (int i = testArray.length - 1; i >= 0; i--) {
