20c94b8d651ce99f7bb2c36d2640179fcd011a38
PowerlineR413 <Powerline413@yandex.ru>
2024-08-27 18:19:27 +0300

    Определитель чётности числа 1.0

--- Lesson9/Task 2/src/Main.java
+++ Lesson9/Task 2/src/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        numero();
     }
+    public static void numero(){
+        System.out.print("""
+                Добро пожаловать в программу Определитель Чётности ! Введите любое число, и 
+                наша программа определит чётное оно или нет !
+                Пожалуйста, введите число:
+                """);
+        int a = new Scanner(System.in).nextInt();
+        if (a % 2 == 0){
+            System.out.println(true);
+        }
+        else if (a % 2 != 0){
+            System.out.println(false);
+        }
 
+    }
 }
