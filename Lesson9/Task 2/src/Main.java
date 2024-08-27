7f8ba5a07245d1e563dacc4e7830a1d915edcfe8
PowerlineR413 <Powerline413@yandex.ru>
2024-08-27 18:32:28 +0300

    Определитель чётности числа 1.1 Разделил логику на два метода

--- Lesson9/Task 2/src/Main.java
+++ Lesson9/Task 2/src/Main.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int scanInput;
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
         numero();
+        evenOrNot();
     }
     public static void numero(){
         System.out.print("""
-                Добро пожаловать в программу Определитель Чётности ! Введите любое число, и 
+                Добро пожаловать в программу Определитель Чётности ! Введите любое число, и
                 наша программа определит чётное оно или нет !
                 Пожалуйста, введите число:
                 """);
-        int a = new Scanner(System.in).nextInt();
-        if (a % 2 == 0){
+         scanInput = new Scanner(System.in).nextInt();
+    }
+    public static void evenOrNot(){
+        if (scanInput % 2 == 0){
             System.out.println(true);
         }
-        else if (a % 2 != 0){
+        else if (scanInput % 2 != 0){
             System.out.println(false);
         }
-
     }
 }
